17964a0f4b81b87484b0fe412c6d13e33f06db46
kalnynsh <kda869@yandex.ru>
2017-06-05 17:39:22 +0300

    In main/view revise class HomeView using data from form and search.

--- main/views.py
+++ main/views.py
@@ -1,16 +1,31 @@
-import random
+from django.shortcuts import render
+# from django.template.response import RequestContext
+from django.views.generic import View
 
-from django.views.generic import TemplateView
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.connections import connections
 
-from main.models import Product
+from main.forms import SearchForm
 
 
-class HomeView(TemplateView):
-    template_name = "main/home.html"
-
-    def get_context_data(self, **kwargs):
-        ctx = super(HomeView, self).get_context_data(**kwargs)
-
-        ctx['products'] = random.sample(list(Product.objects.all()), 50)
-
-        return ctx
+class HomeView(View):
+    
+    def get(self, request):
+        
+        form = SearchForm(request.GET)
+        
+        ctx = {
+            "form": form
+        }
+    
+        if form.is_valid():
+            connections.create_connection()
+            
+            name_query = form.cleaned_data["name"]
+            s = Search(index="daintree").query("match", name=name_query)
+            
+            result = s.execute()
+            
+            ctx["products"] = result.hits
+            
+        return render(request, "main/home.html", ctx)
