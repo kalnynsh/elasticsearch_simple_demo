60136b231fea64ba9630d43042025ea1fc06b0d1
kalnynsh <kda869@yandex.ru>
2017-06-05 20:19:14 +0300

    In main/forms add fields with min max value.

--- main/forms.py
+++ main/forms.py
@@ -3,3 +3,5 @@ from django import forms
 
 class SearchForm(forms.Form):
     name = forms.CharField(required=False)
+    min_price = forms.IntegerField(required=False, label="Minimum Price")
+    max_price = forms.IntegerField(required=False, label="Maximum Price")
