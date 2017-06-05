f10cf0962c3215c0220a57c9363cbc9e676099ec
kalnynsh <kda869@yandex.ru>
2017-06-05 20:29:54 +0300

    In main/views in HomeView add min, max price rules for search.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-# from django.template.response import RequestContext
 from django.views.generic import View
 
 from elasticsearch_dsl import Search
@@ -18,12 +17,26 @@ class HomeView(View):
         }
     
         if form.is_valid():           
-            name_query = form.cleaned_data["name"]
+            name_query = form.cleaned_data.get("name")
             
             if name_query:
                 s = Search(index="daintree").query("match", name=name_query)
             else:
                 s = Search(index="daintree")
+            
+            min_price = form.cleaned_data.get("min_price")
+            max_price = form.cleaned_data.get("max_price")
+            
+            if min_price is not None or max_price is not None:
+                price_query = dict()
+                
+                if min_price is not None:
+                    price_query["gte"] = min_price
+                    
+                if max_price is not None:
+                    price_query["lte"] = max_price
+                
+                s = s.query("range", price=price_query)
             
             result = s.execute()
             
