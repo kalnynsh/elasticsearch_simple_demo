e28cd9e33713212a66e4c714817dc63d28310594
kalnynsh <kda869@yandex.ru>
2017-06-06 19:04:32 +0300

    In /main/es_docs change category = String(required=True, fielddata=True, index="not_analyzed"), add fielddata and index.

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, fielddata=True)
+    name = models.CharField(max_length=100)
     description = models.TextField()
 
 
